ce12d630d915d18f38d0be8128baf21392826399
Kyle Leonard <kdleonard@icloud.com>
2020-02-12 13:27:32 -0600

    Started working on the server calls.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db");
+
+const server = express();
+
+server.post("/api/users", (req, res) => {
+  const id = req.params.id;
+  const newUser = {
+    name: req.body.name,
+    bio: req.body.bio
+  };
+});
