e34e3038dddca38eac8ea89bf8309fc7e01ebfbf
Kyle Leonard <kdleonard@icloud.com>
2020-02-12 20:11:59 -0600

    Finished the post req
    Getting errors though

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const db = require("./data/db");
 const server = express();
 
 server.post("/api/users", (req, res) => {
-  const id = req.params.id;
   const newUser = {
+    id: users.length + 1,
     name: req.body.name,
     bio: req.body.bio
   };
+  users.push(newUser);
+
+  res.status(201).json({ message: "User not found" });
+});
+
+const port = 8080;
+
+server.listen(port, () => {
+  console.log(`server started at http://localhost${port}`);
 });
