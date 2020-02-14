33b7343a0f04701c4441aa4b4d11e8a79f472860
Kyle Leonard <kdleonard@icloud.com>
2020-02-13 18:27:28 -0600

    Update to POST

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const express = require("express");
 const db = require("./data/db");
 
 const server = express();
+server.use(express.json());
 
 server.post("/api/users", (req, res) => {
   const newUser = {
-    id: db.insert(user),
+    // id: db.insert(user),
     name: req.body.name,
     bio: req.body.bio
   };
-  users.push(newUser);
-
+  // users.push(newUser);
+  db.insert(newUser);
   res.status(201).json({ message: "User not found" });
+  // if (users !== name || bio) {
+  //   return res.status(400).json({ message: "Bad Request" });
+  // }
 });
 
 const port = 8080;
