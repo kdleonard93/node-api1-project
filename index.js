5472b7c3308d420914734fe74a3a005deb2551ff
Kyle Leonard <kdleonard@icloud.com>
2020-02-16 13:04:05 -0600

    Completed first get req

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const express = require("express");
 const db = require("./data/db");
 
 const server = express();
+const port = 8080;
+
 server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(data => res.json(data))
+    .catch(data => res.status(500).json({ message: "User not found." }));
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+});
 
 server.post("/api/users", (req, res) => {
   const newUser = {
@@ -18,8 +30,6 @@ server.post("/api/users", (req, res) => {
   //   return res.status(400).json({ message: "Bad Request" });
   // }
 });
-
-const port = 8080;
 
 server.listen(port, () => {
   console.log(`server started at http://localhost${port}`);
