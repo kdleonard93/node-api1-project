a2d60253f2405316b6239c8bbe015859c6ad657a
Kyle Leonard <kdleonard@icloud.com>
2020-04-08 18:29:10 -0500

    Post call

--- index.js
+++ index.js
@@ -26,3 +26,20 @@ server.get("/api/users/:id", (req, res) => {
     })
     .catch((data) => res.status(500).json({ message: "User not found." }));
 });
+
+server.post("/api/users", (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    return res
+      .status(500)
+      .json({ message: "Please provide name and bio for user." });
+  }
+  db.insert({ name, bio })
+    .then((res) => db.findById(res.id))
+    .then((data) => res.status(201).json(data))
+    .catch((error) =>
+      res
+        .status(500)
+        .json({ message: "There is an error with saving the user." })
+    );
+});
