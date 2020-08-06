4c3e0f65d39612a8742282654f168fb96841ff79
Kyle Leonard <kdleonard@icloud.com>
2020-08-05 19:26:00 -0500

    Moved server to index

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db");
+
+const port = 8080;
+
+const server = express();
+server.use(express.json());
+
+server.get("/users", (req, res) => {
+    const users = db.find();
+    res.json(users);
+})
+
+server.listen(port, () => {
+    console.log(`server started at http://localhost${port}`);
+  });
